bcfbc252419c12029ec0f853e679557d459f00ad
Mohamed Badr <eng.ma.badr@gmail.com>
2024-03-20 15:06:50 +0300

    clone the repo

--- main.ps1
+++ main.ps1
@@ -1,8 +1,14 @@
-﻿# delete local repository
+﻿#fixed variables
+$remoteRepositoryUrl = "https://Twaijrigcs@dev.azure.com/Twaijrigcs/Wafi/_git/Wafi"
+$localRepositoryPath = "C:\Storage\publisher-temp"
 
 # ask user for the branch name
+$brnachName = Read-Host "Enter remote branch name"
 
 # clone remote repository
+Write-Host "Start cloning the remote repository..."
+git clone --branch $brnachName $remoteRepositoryUrl $localRepositoryPath
+Write-Host "Repository has been cloned successfully"
 
 # check if dn migration is needed
 
@@ -15,4 +21,6 @@
 # copy the application to the production server
 
 # copy the build to the production server
+
+# delete local source code and publish folders
 
