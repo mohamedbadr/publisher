60bbf1239e01b925d7e728eb246c9d6b143746fd
Mohamed Badr <eng.ma.badr@gmail.com>
2024-03-20 14:37:24 +0300

    first commit

---
+++ main.ps1
@@ -0,0 +1,18 @@
+﻿# delete local repository
+
+# ask user for the branch name
+
+# clone remote repository
+
+# check if dn migration is needed
+
+# build the application
+
+# build angular application
+
+# backup the current build on the production server
+
+# copy the application to the production server
+
+# copy the build to the production server
+
